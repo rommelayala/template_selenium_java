b87cd416f85d82a86d003e2d9a5ca68df04dde45
rommelayala <rommel.ayala@gmail.com>
2024-08-30 12:44:40 +0200

    elimine el AbstractWebdriverBase y pase la responsabilidad a Testbase directamente

--- src/test/java/com/example/testScripts/SeleniumSpec.java
+++ src/test/java/com/example/testScripts/SeleniumSpec.java
@@ -3,20 +3,21 @@ package com.example.testScripts;
 import com.example.properties.Environment;
 import org.openqa.selenium.WebDriver;
 
-public class SeleniumSpec extends AbstractWebdriverBase {
+public class SeleniumSpec extends TestBase {
+  /**
+   * Obtiene el webdriver desde TestBase, podria obtenerlo directamente porque es estatico pero por
+   * legibilidad creo el atributo
+   */
+  WebDriver webDriver = getDriver();
 
-  public SeleniumSpec() {
-    super(TestBase.getDriver());
-  }
+  public SeleniumSpec() {}
 
   /**
    * Mostrar valores de un atributo System.out.println("EL valor del nuevo atributo es -> " +
    * Environment.getProperties().toto()); *
    */
   public void openBrowserUsingProperties() {
-    driver.get(Environment.getProperties().url());
-    // Mostrar valores de un atributo
-    // System.out.println("EL valor del nuevo atributo es -> " +
-    // Environment.getProperties().toto());
+    webDriver.get(Environment.getProperties().url());
+
   }
 }
