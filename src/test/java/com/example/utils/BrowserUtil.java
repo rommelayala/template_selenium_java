0289dd29534118a47276946fc3a9dbef36ce5ae2
rommelayala <rommel.ayala@gmail.com>
2024-09-04 12:44:18 +0200

    añado webdrivermanager

--- src/test/java/com/example/utils/BrowserUtil.java
+++ src/test/java/com/example/utils/BrowserUtil.java
@@ -1,5 +1,6 @@
 package com.example.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -13,22 +14,27 @@ public class BrowserUtil {
         WebDriver driver = null;
         switch (Browser.valueOf(browser.toUpperCase())) {
             case CHROME:
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("-disable-search-engine-choice-screen");
                 driver = new ChromeDriver(chromeOptions);
                 break;
             case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
             case EDGE:
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 break;
             case CHROME_HEADLESS:
+                WebDriverManager.chromedriver().setup();
                 ChromeOptions chromeOptions_headless = new ChromeOptions();
                 chromeOptions_headless.addArguments("--headless");
                 driver = new ChromeDriver(chromeOptions_headless);
                 break;
             case FIREFOX_HEADLESS:
+                WebDriverManager.firefoxdriver().setup();
                 FirefoxOptions options = new FirefoxOptions();
                 options.addArguments("-headless");
                 driver = new FirefoxDriver(options);
